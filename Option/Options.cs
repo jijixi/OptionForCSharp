11efd6f91c56b48656c8303dfd70b46abe0cae39
jijixi <gotow@nifty.com>
2012-08-08 12:08:21 +0900

    Initial commit of Options.
    ML like option types.

---
+++ Option/Options.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectUtility
+{
+    public static class Options
+    {
+        public static Option<T> make<T>(Func<T> f)
+        {
+            try {
+                T v = f();
+                if (v == null) {
+                    return new None<T>();
+                } else {
+                    return new Some<T>(v);
+                }
+            } catch (Exception) {
+                return new None<T>();
+            }
+        }
+
+        public class Option<T> : IEnumerable<T>
+        {
+            public virtual bool IsAvailable
+            {
+                get { return false; }
+            }
+
+            public virtual T value
+            {
+                get { return default(T); }
+            }
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class None<T> : Option<T>
+        {
+        }
+
+        public class Some<T> : Option<T>
+        {
+            public Some(T v)
+            {
+                this.value_ = v;
+            }
+
+            override public bool IsAvailable
+            {
+                get { return true; }
+            }
+
+            private T value_;
+            override public T value
+            {
+                get
+                {
+                    return this.value_;
+                }
+            }
+        }
+    }
+}
---
+++ OptionTests/OptionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace OptionTests
+{
+    using ObjectUtility;
+
+    [TestClass]
+    public class OptionTest
+    {
+        [TestMethod]
+        public void TestMakeFunction()
+        {
+            var none = Options.make<string>(() => null);
+            Assert.IsInstanceOfType(none, typeof(Options.None<string>));
+            Assert.IsFalse(none.IsAvailable);
+            var some = Options.make(() => "This" + " is" + "test.");
+            Assert.IsInstanceOfType(some, typeof(Options.Some<string>));
+            Assert.IsTrue(some.IsAvailable);
+            string s = null;
+            none = Options.make<string>(() =>
+            {
+                if (s == null) {
+                    throw new Exception();
+                } else {
+                    return s;
+                }
+            });
+            Assert.IsInstanceOfType(none, typeof(Options.None<string>));
+            Assert.IsFalse(none.IsAvailable);
+        }
+    }
+}
